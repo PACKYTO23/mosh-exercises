567b12d12d77f51c200855f0a9a55cdbdfa1ad47
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2024-01-26 16:08:09 -0700

    Add files via upload

---
+++ new.py
@@ -0,0 +1,57 @@
+# print("Mosh Hamedani")
+# print("o----")
+# print(" ||||")
+# print("*" * 10)
+
+# / # / # / # / # / #
+
+# price = 10
+# rating = 4.9
+# name = "Mosh"
+# is_published = False
+
+# print(price)
+
+# full_name = "John Smith"
+# age = 20
+# is_new = True
+
+# name = input("What is your name? ")
+# print("Hi " + name)
+
+# name = input("What is your name? ")
+# color = input("What is your favorite color? ")
+# print(name + " likes " + color + ".")
+
+# / # / # / # / # / #
+
+# birth_year = input("Birth year: ")
+# print(type(birth_year))
+# age = 2024 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# / # / # / # / # / #
+
+# pounds = input("What is your weight in pounds?: ")
+# kilograms = "{:.2f}".format(int(pounds) / 2.205)
+# print(kilograms)
+
+# / # / # / # / # / #
+
+# course = "Python for Beginners"
+# course = "Python's for Beginners"
+# course = 'Python for "Beginners"'
+# course = '''
+# Hi John,
+
+# Here is our first email to you.
+
+# Thank you.
+# The support team
+# '''
+
+# name = "Jennifer"
+# print(name[1:-1])
+
+# / # / # / # / # / #
